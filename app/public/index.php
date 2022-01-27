83494fe081c8d3c434680372d7c766c0ce1fc129
Tom Hooper <tom.hooper@mobihq.com>
2022-01-27 13:40:11 +1300

    continue setup

--- app/public/index.php
+++ app/public/index.php
@@ -1,3 +1,109 @@
-<?php
-    include '../config/database.php';
-?>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>PDO - Read Records - PHP CRUD Tutorial</title>
+
+    <!-- Latest compiled and minified Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+
+    <!-- custom css -->
+    <style>
+        .m-r-1em{ margin-right:1em; }
+        .m-b-1em{ margin-bottom:1em; }
+        .m-l-1em{ margin-left:1em; }
+        .mt0{ margin-top:0; }
+    </style>
+
+</head>
+<body>
+
+<!-- container -->
+<div class="container">
+
+    <div class="page-header">
+        <h1>Read Products</h1>
+    </div>
+
+    <?php
+    // include database connection
+    include 'config/database.php';
+
+    // delete message prompt will be here
+
+    // select all data
+    $query = "SELECT id, name, description, price FROM products ORDER BY id DESC";
+    $stmt = $con->prepare($query);
+    $stmt->execute();
+
+    // this is how to get number of rows returned
+    $num = $stmt->rowCount();
+
+    // link to create record form
+    echo "<a href='create.php' class='btn btn-primary m-b-1em'>Create New Product</a>";
+
+    //check if more than 0 record found
+    if($num>0){
+
+        //start table
+        echo "<table class='table table-hover table-responsive table-bordered'>";
+
+        //creating our table heading
+        echo "<tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Description</th>
+        <th>Price</th>
+        <th>Action</th>
+    </tr>";
+
+        // retrieve our table contents
+// fetch() is faster than fetchAll()
+// http://stackoverflow.com/questions/2770630/pdofetchall-vs-pdofetch-in-a-loop
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+            // extract row
+            // this will make $row['firstname'] to
+            // just $firstname only
+            extract($row);
+
+            // creating new table row per record
+            echo "<tr>
+        <td>{$id}</td>
+        <td>{$name}</td>
+        <td>{$description}</td>
+        <td>${$price}</td>
+        <td>";
+            // read one record
+            echo "<a href='read_one.php?id={$id}' class='btn btn-info m-r-1em'>Read</a>";
+
+            // we will use this links on next part of this post
+            echo "<a href='update.php?id={$id}' class='btn btn-primary m-r-1em'>Edit</a>";
+
+            // we will use this links on next part of this post
+            echo "<a href='#' onclick='delete_user({$id});'  class='btn btn-danger'>Delete</a>";
+            echo "</td>";
+            echo "</tr>";
+        }
+
+// end table
+        echo "</table>";
+
+    }
+
+// if no records found
+    else{
+        echo "<div class='alert alert-danger'>No records found.</div>";
+    }
+    ?>
+
+</div> <!-- end .container -->
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+
+<!-- Latest compiled and minified Bootstrap JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<!-- confirm delete record will be here -->
+
+</body>
+</html>
